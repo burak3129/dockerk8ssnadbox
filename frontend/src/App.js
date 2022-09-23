479419260ce633e9b175092a9894b96b6290eba7
Burak Cetin <>
2022-09-23 10:38:59 +0200

    frontend web site for favs

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>SOUTHERN-CHARMS</p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="http://www.southern-charms2.com/sandra/photos.htm"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          SANDRA
+        </a>
+        <a
+          className="App-link"
+          href="http://www.southern-charms3.com/raven/photos.htm"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          RAVEN
         </a>
       </header>
     </div>
